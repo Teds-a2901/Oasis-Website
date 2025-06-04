b2c0879ef236f02895cd8fa642b84ee8adf08459
Teds <marktedra29@gmail.com>
2025-06-04 15:22:26 +0800

    do a thing
    Organize all the folder
    styling with tailwindcss

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,9 @@
-export default function Page() {
+import Counter from "@/app/_components/Counter";
+
+export default async function Page() {
   return (
     <div>
-      <h1>The wild Oasis. Welcome to paradise</h1>;
+      <h1>The wild Oasis. Welcome to paradise</h1>
     </div>
   );
 }
