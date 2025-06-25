03a12f83ca166cfbb9f3094e011b8d46192673d4
Teds <marktedra29@gmail.com>
2025-06-25 13:14:42 +0800

    fixed

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -1,9 +1,9 @@
-import Navigation from "../_components/Navigation";
-import Logo from "../_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
-    <header className="border-b border-primary-900 px-8 py-3">
+    <header className="border-b border-primary-900 px-8 py-5">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />
--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,17 +1,18 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/Logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image
-        src="/Logo.png"
+        src={logo}
+        height="60"
         quality={100}
-        height="55"
-        width="55"
+        width="60"
         alt="The Wild Oasis logo"
       />
-      <span className="text-lg font-semibold text-primary-100">
+      <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
     </Link>
