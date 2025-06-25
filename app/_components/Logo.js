86510e7b31b1f77f660bc12d988067a163d2a743
Teds <marktedra29@gmail.com>
2025-06-25 13:25:35 +0800

    debug fixed for deployment

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/Logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image
-        src={logo}
+        src="/Logo.png"
         height="60"
         quality={100}
         width="60"
