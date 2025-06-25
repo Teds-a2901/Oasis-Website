4e0933624ce71d82f035c110a76c0f615e7f5281
Teds <marktedra29@gmail.com>
2025-06-25 13:00:45 +0800

    do a thing image fixed

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
+import Logo from "./app/_components/Logo.js";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image
-        src="/Logo.png"
+        src="Logo"
         quality={100}
         height="55"
         width="55"
